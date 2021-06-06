41cc92cdae5346ef349f478621a7dd926c3971a4
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-06-07 00:52:16 +0300

    REFACTORING

--- src/bot_proseo/settings.py
+++ src/bot_proseo/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+if os.environ.get('SECRET_KEY'):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+else:
+    SECRET_KEY = 'django-insecure-keyfi2ej208urf2j03#j032j0f38!0nf1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
