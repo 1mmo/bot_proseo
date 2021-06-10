9ed91ea0f54017ad6ef8315251e9038c1148dd54
korneev.r <rftgyhuj123987>
2021-06-10 18:15:04 +0300

    DEBUG: parsing db message

--- src/bot/common/db.py
+++ src/bot/common/db.py
@@ -148,7 +148,7 @@ def chat_ids():
     FROM users
     WHERE subscribe=TRUE
     """
-    connection.autocommit = True
+    connection.autocommit=True
     cursor = connection.cursor()
     cursor.execute(query, connection)
     rows = cursor.fetchall()
@@ -207,6 +207,9 @@ def get_not_published_posts():
     posts = []
     for res in result:
         get_text = res[1:-1].split('"')
+        while ',' in get_text:
+            get_text.remove(',')
+        get_text.remove
         logging.error('GET_TEXT')
         logging.error(get_text)
         created_at = get_text[1]
